cacda2df371ea17db979eb9fe3fb13dad61bef17
Lee Quarella <leequarella@gmail.com>
2020-05-30 20:17:32 -0400

    Slightly less ugly

--- flow_meter.ino
+++ flow_meter.ino
@@ -1,18 +1,14 @@
-int flowPin1 = 2;    //This is the input pin on the Arduino
-int flowPin2 = 3;    //This is the input pin on the Arduino
+int flowPin = 3;    //This is the input pin on the Arduino
 
-volatile int totalPulses1 = 0; //volatile to ensure updates correctly during interrupt
-volatile int totalPulses2 = 0;
+volatile int totalPulses = 0;
 int loopCount;
-int oldPulse2 = 0;
+int oldPulse = 0;
 
 void setup() {
   Serial.begin(9600);
-  //pinMode(flowPin1, INPUT);
-  pinMode(flowPin2, INPUT);
+  pinMode(flowPin, INPUT);
 
-  //attachInterrupt(digitalPinToInterrupt(flowPin1), Flow, CHANGE);  //Configures interrupt 0 (pin 2 on the Arduino Uno) 
-  attachInterrupt(digitalPinToInterrupt(flowPin2), Flow2, CHANGE);  //Configures interrupt 1 (pin 3 on the Arduino Uno) 
+  attachInterrupt(digitalPinToInterrupt(flowPin), Flow, CHANGE);  //Configures interrupt 1 (pin 3 on the Arduino Uno) 
 }
 
 void loop() {
@@ -25,27 +21,18 @@ void loop() {
     Serial.print(".");
   }
 
-  if(oldPulse2 != totalPulses2) {
+  if(oldPulse != totalPulses) {
+    int diff = totalPulses - oldPulse;
+    oldPulse = totalPulses;
     Serial.println("");
-    Serial.println((String)"Pulse from pin" + flowPin2 + "."); 
-    int diff = totalPulses2 - oldPulse2;
+    Serial.println((String)"Pulse from pin" + flowPin + ".");
     Serial.println((String)"Difference: " + diff);
-    Serial.println((String)"New Total: " + totalPulses2);
-    oldPulse2 = totalPulses2;
+    Serial.println((String)"New Total: " + totalPulses);
   }
-  delay (30);
-}
- 
-void Flow()
-{
-  totalPulses1++; 
-  Serial.println("");
-  Serial.println((String)"Flow Pin 1: " + totalPulses1);
+  delay (60);
 }
 
-void Flow2()
+void Flow()
 {
-  totalPulses2++; 
-  //Serial.println("");
-  //Serial.println((String)"Flow Pin 2: " + totalPulses2);
+  totalPulses++;
 }
